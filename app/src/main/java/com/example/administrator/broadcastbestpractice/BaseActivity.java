4847c395044dc57d0e29f3da7dc19709e969f4e3
spody <592504835@qq.com>
2017-01-24 14:15:29 +0800

    实现强制下线功能

---
+++ app/src/main/java/com/example/administrator/broadcastbestpractice/BaseActivity.java
@@ -0,0 +1,59 @@
+package com.example.administrator.broadcastbestpractice;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2017/1/24.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+    private ForceofflineReceiver receiver;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityCollector.addActivity(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("com.example.administrator.broadcastbest.FORCE_OFFLINE");
+        receiver = new ForceofflineReceiver();
+        registerReceiver(receiver,intentFilter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ActivityCollector.removeActivity(this);
+    }
+
+    private class ForceofflineReceiver extends BroadcastReceiver{
+        @Override
+        public void onReceive(final Context context, Intent intent) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(context);
+            builder.setTitle("Warning");
+            builder.setMessage("你已经强制下线，请重新登录。");
+            builder.setCancelable(false);
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    ActivityCollector.finishAll();
+                    Intent intent = new Intent(context,LoginActivity.class);
+                    context.startActivity(intent);
+                }
+            });
+            builder.show();
+        }
+    }
+}
